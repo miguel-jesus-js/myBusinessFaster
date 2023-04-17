4bd960e3e1a262d71759aa1db78ee9791588601c
Miguel López <miguellopez@softcode.com.mx>
2023-04-17 01:58:46 -0600

    - cambio texto de pestaña

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>Inicio de sesión</title>
+    <title>INICIO DE SESIÓN</title>
     <link rel="icon" href="{{ asset('img/logo-icono.ico') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
